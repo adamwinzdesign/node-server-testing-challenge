bdb9f0fb2e3a6b072770b1ab57852c6cf9ed9e6a
adamwinzdesign <adamwinzdesign@gmail.com>
2020-02-27 17:17:52 -0600

    basic server structure

---
+++ auth/authRouter.js
@@ -0,0 +1,60 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Users = require('../users/usersModel');
+const { jwtSecret } = require('../config/secrets.js');
+
+// register
+router.post('/register', (req, res) => {
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
+});
+
+// login
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({
+          message: `Welcome, ${user.username}!`,
+          token
+        })
+      } else {
+        res.status(401).json({ message: 'Invalid credentials!' })
+      }
+    })
+    .catch(error => {
+      console.log('Error: ', error);
+      res.status(500).json({ error: 'Error logging in!' })
+    })
+})
+
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+    role: user.role || 'user'
+  }
+
+  const options = {
+    expiresIn: '1h'
+  }
+
+  return jwt.sign(payload, jwtSecret, options)
+}
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,4 @@
+const server = require('./api/server.js');
+
+const port = process.send.PORT || 5000;
+server.listen(port, () => console.log(`** Server up on port ${port}! **`))
